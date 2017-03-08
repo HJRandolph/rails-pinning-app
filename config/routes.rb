649c3383e8d7c67f1f1d1df0b2781ed65976cea1
HJRandolph <hjrandolph@gmail.com>
2017-03-07 23:28:16 -0500

    Deploying to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pins#index'
 
+  get '/new', to: 'pins#new'
+  
   resources :pins
     
   # Example of regular route:
