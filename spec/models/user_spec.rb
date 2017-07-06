6f740e0c43b0af4966725eb5f56958521f91e2d0
HJRandolph <hjrandolph@gmail.com>
2017-07-05 20:58:15 -0400

    Testing fails if [:login] is included. Login fails if it is not

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,3 +3,22 @@ require 'spec_helper'
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe "User authenticate method" do
+	before(:all) do
+ 	 @user = User.create(email: "coder@skillcrush", password: "password")
+	end
+ 
+	after(:all) do
+	  if !@user.destroyed?
+ 	   @user.destroy
+  	end
+end
+
+ 
+it 'authenticates and returns a user when valid email and password passed in' do
+	authenticate_user = User.authenticate(@user.email, @user.password)
+	expect(authenticate_user).to eql(@user)
+end
+
+end
